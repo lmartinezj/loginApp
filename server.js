001b81056f33b659d2e982d9a414b84485094734
lmartinez <lmartinezj@gmail.com>
2022-03-13 23:15:24 -0500

    set  cookie as h1 output

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     console.log('state: ' + req.query.state)
     res.cookie('authCode', req.query.code)
     res.cookie('state', req.query.state)
-    res.render('index.ejs', {name: 'authCode'})
+    res.render('index.ejs', {name: req.query.code})
 })
 
 app.get('/login', (req, res) => {
