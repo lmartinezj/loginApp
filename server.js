74e2f2a377265482eaa16a3a721d5cd0eba515c5
lmartinez <lmartinezj@gmail.com>
2022-03-17 09:05:24 -0500

    installed java-caller added comments for roadmap on feedback

--- server.js
+++ server.js
@@ -76,6 +76,56 @@ app.post('/token', (req, res) => {
 
 })
 
+app.post('/session-feedback', (req, res) => {
+    //res.send("Auth code: ")    
+    const token = req.body.token
+    console.log('ACCESS_TOKEN in //session-feedback request: ' + token)
+    console.log("/session-feedback called")
+
+    const accessToken = process.env.CLIENT_ID
+    const clientSecret = process.env.CLIENT_SECRET
+    const redirect_uri = process.env.REDIRECT_URI
+
+    console.log("access_token: " + token);
+    console.log("bindid_alias: " + clientSecret);
+    console.log("authentication_time: " + redirect_uri);
+    console.log("feedback_auth_value: " + redirect_uri);
+
+    /*
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Host", "signin.bindid-sandbox.io");
+    */
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "authorization_code");
+    urlencoded.append("code", authCode);
+    urlencoded.append("redirect_uri", redirect_uri);
+    urlencoded.append("client_id", clientId);
+    urlencoded.append("client_secret", clientSecret);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'signin.bindid-sandbox.io'
+        },
+        body: urlencoded,
+        redirect: 'follow',
+        mode: 'no-cors'
+    }
+
+    fetch("https://signin.bindid-sandbox.io/token", requestOptions)
+    .then(response => response.json())
+    //.then(result => res.json(result))
+    .then(result => {
+        console.log(result)
+        const myToken = JSON.stringify(result)
+        res.render('token.ejs', { token: myToken })
+    })
+    .catch(error => console.log('error', error))
+
+})
 /*
 app.get('/token', (req, res) => {
     console.log('this is GET /token')
