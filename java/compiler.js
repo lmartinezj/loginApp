582cd8310ca2421198df52f0b20f0caffa6635c9
lmartinez <lmartinezj@gmail.com>
2022-03-18 00:48:42 -0500

    set classpath

--- java/compiler.js
+++ java/compiler.js
@@ -9,6 +9,7 @@ exports.java = function (srcfile, clientSecret, bindIdAccessToken) {
     if (extension === ".java") {
         var args_compile = [];
         args_compile[0] = srcfile;
+        args_compile[0] = '-cp java/*';
         var args_run = [];
         args_run[0] = filename;
         args_run[1] = clientSecret;
