02584972f85a18f26f128da95ef83a914f1be774
lmartinez <lmartinezj@gmail.com>
2022-03-18 08:24:45 -0500

    edit path for main

--- java/run_java.js
+++ java/run_java.js
@@ -3,5 +3,5 @@ var compiler = require('./compiler');
  
 // add comment
 exports.java = function (clientSecret, bindIdAccessToken) {
-    compiler.java('java/Main.java', clientSecret, bindIdAccessToken);
+    compiler.java('./java/Main.java', clientSecret, bindIdAccessToken);
 }
